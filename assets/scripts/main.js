0d02d2c565ea5fb51fc8f2165e7c0ec11d51e3c1
Jonathan Sigg <jonathan.sigg@hotmail.com>
2016-06-23 01:03:13 +0200

    modal for instructions

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
   $('#play').click(function(){
     $('body').attr('class','starting-game');
   });
+
+  $('#instruct').click(function(){
+    $('#modal-instruct').addClass('show');
+  });
+
+  $('.modal').each(function(){
+    var $modal = $(this);
+    $(this).find('.modal-back').click(function(){
+      $modal.removeClass('show');
+    });
+    $(this).find('.modal-close').click(function(){
+      $modal.removeClass('show');
+    });
+  });
 });
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,2 +1,2 @@
-$(document).ready(function(){$("#play").click(function(){$("body").attr("class","starting-game")})});
+$(document).ready(function(){$("#play").click(function(){$("body").attr("class","starting-game")}),$("#instruct").click(function(){$("#modal-instruct").addClass("show")}),$(".modal").each(function(){var c=$(this);$(this).find(".modal-back").click(function(){c.removeClass("show")}),$(this).find(".modal-close").click(function(){c.removeClass("show")})})});
 //# sourceMappingURL=main.js.map
